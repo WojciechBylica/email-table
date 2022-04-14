b67c501a484cb8b096161787afa28b2de1ac243f
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-14 20:17:48 +0200

    Modal moved to parent comp, added propstyles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,7 @@ import { addMail } from '../../pages/EmailTable/emailTableSlice'
 import { nanoid } from '@reduxjs/toolkit'
 import { StyledForm, StyledLabel } from './styled'
 import Button from '../Button'
-import Modal from '../Modal'
+import PropStyles from 'prop-types'
 
 const Form = ({ handleCloseModal }) => {
   const [title, setTitle] = useState('')
@@ -39,40 +39,43 @@ const Form = ({ handleCloseModal }) => {
   }
 
   return (
-    <Modal handleCloseModal={handleCloseModal}>
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledLabel>
-          Tytuł
-          <input
-            name="title"
-            value={title}
-            onChange={handleTitleChange}
-            onBlur={handleTitleBlur}
-            type="text"
-            min="3"
-            max="25"
-            required
-            placeholder="tytuł"
-          />
-        </StyledLabel>
-        <StyledLabel>
-          Wiadomość
-          <textarea
-            name="message"
-            value={message}
-            onChange={handleMessageChange}
-            onBlur={handleMessageBlur}
-            type="textArea"
-            required
-            placeholder="treść wiadomości"
-          />
-        </StyledLabel>
-        <div>
-          <Button label="dodaj" />
-        </div>
-      </StyledForm>
-    </Modal>
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledLabel>
+        Tytuł
+        <input
+          name="title"
+          value={title}
+          onChange={handleTitleChange}
+          onBlur={handleTitleBlur}
+          type="text"
+          min="3"
+          max="25"
+          required
+          placeholder="tytuł"
+          autoFocus
+        />
+      </StyledLabel>
+      <StyledLabel>
+        Wiadomość
+        <textarea
+          name="message"
+          value={message}
+          onChange={handleMessageChange}
+          onBlur={handleMessageBlur}
+          type="textArea"
+          required
+          placeholder="treść wiadomości"
+        />
+      </StyledLabel>
+      <div>
+        <Button label="dodaj" />
+      </div>
+    </StyledForm>
   )
 }
 
 export default Form
+
+Form.propsStyles = {
+  handleCloseModal: PropStyles.func.isRequired,
+}
