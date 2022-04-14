b1790a0e87532f8674688d48078cfefa0459f508
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-14 17:54:49 +0200

    added new Date to ISOString; styled

--- src/pages/EmailTable/EmailTable.js
+++ src/pages/EmailTable/EmailTable.js
@@ -1,6 +1,6 @@
 import { Button, Table } from '../../components'
 import { useDispatch } from 'react-redux'
-import { ButtonWrapper } from './styled'
+import { ButtonWrapper, TableWrapper } from './styled'
 import { addMail } from './emailTableSlice'
 import { nanoid } from '@reduxjs/toolkit'
 
@@ -8,24 +8,23 @@ const EmailTable = () => {
   const dispatch = useDispatch()
 
   const handleAddMail = () => {
-    console.log('clicked')
     dispatch(
       addMail({
         title: 'testowy tytuł2',
         id: nanoid(),
         text: 'Scrócony text wiadomości lorem ipsum',
-        date: '2022-04-15T01:41:48.151Z',
+        date: new Date().toISOString(),
       })
     )
   }
 
   return (
-    <>
+    <TableWrapper>
       <ButtonWrapper>
         <Button handleClick={handleAddMail} />
       </ButtonWrapper>
       <Table />
-    </>
+    </TableWrapper>
   )
 }
 
