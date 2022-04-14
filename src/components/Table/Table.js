c5db20d7ee76b110a925e476f5d7799ae47c78dd
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-14 04:06:39 +0200

    Table written

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,5 +1,38 @@
+import Button from '../Button'
+
 const Table = () => {
-  return <table>tableka</table>
+  const data = [
+    {
+      title: 'testowy tytuł',
+      id: 'testowy tytuł',
+      text: 'Scrócony text wiadomości lorem ipsum',
+      date: '2022-04-14T01:41:48.151Z',
+    },
+  ]
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th scope="row">Tytuł</th>
+          <th scope="row">Treść wiadomości</th>
+          <th scope="row">Data dodania</th>
+          <th scope="row"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((mail) => (
+          <tr>
+            <td>{mail.title}</td>
+            <td>{mail.text}</td>
+            <td>{mail.date}</td>
+            <td>
+              <Button label="x" remove />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default Table
