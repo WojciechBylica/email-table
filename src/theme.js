19ad994e9c66bf7287701ae17d2a7c3f05378e2c
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-18 00:46:39 +0200

    added cornFlowerBlue color

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,7 @@ export const theme = {
     white: '#ffffff',
     lightGrey: '#e4e2e2',
     grey: '#9e9e9e',
+    cornflowerBlue: '#666bf6b5',
     green: '#0a960a',
     red: '#ff0000',
     lightBlack: '#4043423b',
