30943368aeb2cef45008fafe9b857bcb84109fc5
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-18 02:30:06 +0200

    added prop-types and defaultProps

--- src/components/Iframe/Iframe.js
+++ src/components/Iframe/Iframe.js
@@ -1,5 +1,6 @@
 import { deleteDangerousHTMLTags } from '../../helpers'
 import { StyledIframe } from './styled'
+import PropTypes from 'prop-types'
 
 const Iframe = ({ title, text }) => {
   return (
@@ -11,3 +12,13 @@ const Iframe = ({ title, text }) => {
 }
 
 export default Iframe
+
+Iframe.defaultProps = {
+  title: 'Brak danych',
+  text: 'Brak danych',
+}
+
+Iframe.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+}
