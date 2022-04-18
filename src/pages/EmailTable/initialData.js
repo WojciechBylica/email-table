8baf22063702893d6e779b30f0a54c855e8d8072
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-18 02:29:30 +0200

    deleted not used array

--- src/pages/EmailTable/initialData.js
+++
@@ -1,8 +0,0 @@
-export const initialData = [
-  {
-    title: 'testowy tytuł',
-    id: 'testowy tytuł',
-    text: 'Scrócony text wiadomości lorem ipsum',
-    date: '2022-04-14T01:41:48.151Z',
-  },
-]
